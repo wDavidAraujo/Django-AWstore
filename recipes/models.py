1f0c1f89e91743569813a2e81d84a631bd39c9a7
David Araujo <waldson.david0322@gmail.com>
2024-08-20 23:48:46 -0300

    Fazendo link entre o banco de dados e o front do site.

--- recipes/models.py
+++ recipes/models.py
@@ -3,10 +3,10 @@ from django.db import models
 
 
 class Category(models.Model):
-    nome = models.CharField(max_length=65)
+    name = models.CharField(max_length=65)
 
     def __str__(self):
-        return self.nome
+        return self.name
 
 class Recipe(models.Model):
     title = models.CharField(max_length=65)
@@ -28,3 +28,6 @@ class Recipe(models.Model):
     author = models.ForeignKey(
         User, on_delete=models.SET_NULL, null=True
     )
+
+    def __str__(self):
+        return self.title
