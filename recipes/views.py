48bd309ed57b77c725c70231f5f1d7d6938f337f
David Araujo <waldson.david0322@gmail.com>
2024-09-16 23:41:41 -0300

    Encrementando o Back End para maior agilidade do usuário.

--- recipes/views.py
+++ recipes/views.py
@@ -1,24 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404, get_object_or_404
 from utils.recipes.factory import make_recipe
-from .models import Recipe
+
+from recipes.models import Recipe
 
 def home(request):
-    recipes = Recipe.objects.all().order_by('-id')
+    recipes = Recipe.objects.filter(
+        is_published=True
+    ).order_by('-id')
     return render(request, 'recipes/pages/home.html', context={
         'recipes': recipes,
     })
 
 def category(request, category_id):
-    recipes = Recipe.objects.filter(
-        category__id=category_id
+    recipes = get_list_or_404(
+        Recipe.objects.filter(
+        category__id=category_id,
+        is_published=True,
         ).order_by('-id')
-    return render(request, 'recipes/pages/home.html', context={
+
+    )
+    return render(request, 'recipes/pages/category.html', context={
         'recipes': recipes,
+        'title': f'{recipes[0].category.name}'
     })
 
 
 def recipe(request, id):
+    recipe = Recipe.objects.filter(
+            pk=id,
+            is_published=True,
+        ).order_by('-id').first()
     return render(request, 'recipes/pages/recipe-view.html', context={
-        'recipe': make_recipe(),
+        'recipe': recipe,
         'is_detail_page': True,
     })
