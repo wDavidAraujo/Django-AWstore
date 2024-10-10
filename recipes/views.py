081e8a55c1982f0a3d0b993bc642abcc7ed3990d
David Araujo <waldson.david0322@gmail.com>
2024-10-09 23:19:50 -0300

    Concluindo melhorias de desempenho

--- recipes/views.py
+++ recipes/views.py
@@ -1,12 +1,15 @@
-from django.shortcuts import render, get_list_or_404, get_object_or_404
+from django.shortcuts import render, get_list_or_404, get_object_or_404, render
 from utils.recipes.factory import make_recipe
 
 from recipes.models import Recipe
 
 def home(request):
-    recipes = Recipe.objects.filter(
-        is_published=True
-    ).order_by('-id')
+    recipes = get_list_or_404(
+        Recipe.objects.filter(
+            is_published=True,
+        ).order_by('-id')
+    )
+
     return render(request, 'recipes/pages/home.html', context={
         'recipes': recipes,
     })
@@ -14,22 +17,21 @@ def home(request):
 def category(request, category_id):
     recipes = get_list_or_404(
         Recipe.objects.filter(
-        category__id=category_id,
-        is_published=True,
+            category__id=category_id,
+            is_published=True,
         ).order_by('-id')
 
     )
+
     return render(request, 'recipes/pages/category.html', context={
         'recipes': recipes,
         'title': f'{recipes[0].category.name}'
     })
 
 
-def recipe(request, id):
-    recipe = Recipe.objects.filter(
-            pk=id,
-            is_published=True,
-        ).order_by('-id').first()
+def recipe(request, id):    
+    recipe = get_object_or_404(Recipe,pk=id,is_published=True,)
+
     return render(request, 'recipes/pages/recipe-view.html', context={
         'recipe': recipe,
         'is_detail_page': True,
