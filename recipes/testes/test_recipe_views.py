05cb2395d9b06546117cbfc5fa7383166c3234b4
David Araujo <waldson.david0322@gmail.com>
2024-11-04 23:26:36 -0300

    Organizando e separando os testes

--- recipes/testes/test_recipe_views.py
+++ recipes/testes/test_recipe_views.py
@@ -1,20 +1,6 @@
 from django.test import TestCase
 from django.urls import reverse, resolve
 from recipes import views
- 
-
-class RecipeURLsTest(TestCase):
-    def test_recipe_home_url_is_correct(self):
-        url = reverse('recipes:home')
-        self.assertEqual(url, '/')
-
-    def test_recipe_category_url_is_correct(self):
-        url = reverse('recipes:category', args=(1,))
-        self.assertEqual(url, '/recipes/category/1/')
-        
-    def test_recipe_detail_url_is_correct(self):
-        url = reverse('recipes:recipe', kwargs={'id': 1})
-        self.assertEqual(url, '/recipes/1/')
 
 
 class RecipeViewsTest(TestCase):
@@ -33,4 +19,3 @@ class RecipeViewsTest(TestCase):
             reverse('recipes:recipe', kwargs={'id': 1})
         )
         self.assertIs(view.func, views.recipe)
-    
